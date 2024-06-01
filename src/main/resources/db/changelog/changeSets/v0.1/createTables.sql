2c5127f97a09c3033da39f085f873fa8c1d0e8cf
daniil <daniilkienko@mail.ru>
2024-06-01 16:24:21 +0500

    New Test Products

--- src/main/resources/db/changelog/changeSets/v0.1/createTables.sql
+++ src/main/resources/db/changelog/changeSets/v0.1/createTables.sql
@@ -3,9 +3,10 @@
 -- changeset serhii:create_table_products
 CREATE TABLE products (CategoryId INT NULL, DiscountPrice DECIMAL NULL, Price DECIMAL NULL,
                        ProductId INT AUTO_INCREMENT NOT NULL, CreatedAt datetime NULL,
-                       UpdatedAt datetime NULL, Description VARCHAR(255) NULL,
+                       UpdatedAt datetime NULL, Quantity INT NULL, Description VARCHAR(255) NULL,
                        ImageURL VARCHAR(255) NULL, Name VARCHAR(255) NULL,
                        CONSTRAINT PK_PRODUCTS PRIMARY KEY (ProductId));
+
 
 -- changeset serhii:create_table_categories
 CREATE TABLE categories (CategoryId INT AUTO_INCREMENT NOT NULL,
---
+++ src/test/java/de/telran/warehouse/repository/ProductsRepositoriesTest.java
@@ -0,0 +1,72 @@
+package de.telran.warehouse.repository;
+
+import de.telran.warehouse.entity.Products;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class ProductsRepositoriesTest {
+    private Products testProducts;
+    @Autowired
+    private ProductsRepositories productsRepositories;
+
+    @Test
+    void testGetProduct() {
+        Optional<Products> products = productsRepositories.findById(1L);
+        assertTrue(products.isPresent());
+        assertEquals(1L, products.get().getProductId());
+    }
+
+    @Test
+    void testInsertProduct() {
+        Products testProduct = new Products();
+        testProduct.setName("testProduct");
+        Products newProduct = productsRepositories.save(testProduct);
+        assertNotNull(newProduct);
+        assertTrue(newProduct.getProductId() > 0);
+
+        Optional<Products> findProduct = productsRepositories.findById(newProduct.getProductId());
+        assertTrue(findProduct.isPresent());
+        assertEquals(testProduct.getName(), findProduct.get().getName());
+    }
+
+    @Test
+    void testEditProduct() {
+        Optional<Products> product = productsRepositories.findById(1L);
+        assertTrue(product.isPresent());
+
+        Products getTestProduct = product.get();
+        assertEquals(1L, getTestProduct.getProductId());
+        getTestProduct.setName("Rom");
+        Products newTestProduct = productsRepositories.save(getTestProduct);
+        assertNotNull(newTestProduct);
+        assertEquals(1L, newTestProduct.getProductId());
+
+        Optional<Products> findTestProduct = productsRepositories.findById(1L);
+        assertTrue(findTestProduct.isPresent());
+        assertEquals("Rom", findTestProduct.get().getName());
+    }
+
+    @Test
+    void testDeletedProduct() {
+        Products testProduct = new Products();
+        testProduct.setName("Rom");
+        Products returnTestProduct = productsRepositories.save(testProduct);
+        assertNotNull(returnTestProduct);
+        assertTrue(returnTestProduct.getProductId() > 0);
+
+        Optional<Products> findTestProduct = productsRepositories.findById(returnTestProduct.getProductId());
+        assertTrue(findTestProduct.isPresent());
+        assertEquals(testProduct.getName(), findTestProduct.get().getName());
+
+        productsRepositories.delete(findTestProduct.get());
+
+        Optional<Products> productAfterDelete = productsRepositories.findById(returnTestProduct.getProductId());
+        assertFalse(productAfterDelete.isPresent());
+    }
+}
